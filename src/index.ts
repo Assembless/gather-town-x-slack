9c2e3b4e25076b32f0621df44a3c3649ce4333e0
Mike Eling <mike.eling97@gmail.com>
2022-03-28 01:23:42 +0200

    :wrench: Make first connection

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { Game } from "@gathertown/gather-game-client";
 
-const API_KEY = "LBTb6gfXQq0xUA8K"; //a2ecc1a2d715406756ca6fc9d1354b4cc10cef70cea761011e907ce4dfdcb366
-const SPACE_ID = "c0ajLLNc3ph92hRG";
+const API_KEY = "MQiaWBC2CYrRmUXW";
+const SPACE_ID = "c0ajLLNc3ph92hRG/Assembless";
+const MAP_ID = "rw-6"
+
+const MIKE_PLAYER_ID = "1kZCHNAWcsZXqG1pZ9OzgFwX7jz2";
 
 const main = async () => {
     // create the game object, giving it your API key in this weird way
-    const game = new Game(SPACE_ID, () => Promise.resolve({ apiKey: API_KEY }));
+    const game = new Game(SPACE_ID, () => Promise.resolve({ apiKey: API_KEY }), console.log);
     // this is the line that actually connects to the server and starts initializing stuff
     await game.connect();
     // optional but helpful callback to track when the connection status changes
     // game.subscribeToConnection((connected) => console.log("connected?", connected));
+
+    // game.subscribeToEvent('playerMoves', (v, x) => console.log(JSON.stringify(v, null, 2), JSON.stringify(x, null, 2)));
+    // game.teleport(MAP_ID, 24, 14, "1kZCHNAWcsZXqG1pZ9OzgFwX7jz2")
+    // game.fxShakeCamera(MAP_ID, MIKE_PLAYER_ID, 10, 2000)
 }
 
 main();
