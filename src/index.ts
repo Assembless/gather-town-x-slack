eace0933ba74a0fcd881584ab4c1045783608ddd
Mike Eling <mike.eling97@gmail.com>
2022-03-28 02:03:37 +0200

    :construction: Add @slack/bolt and connect

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import { Game } from "@gathertown/gather-game-client";
+import * as dotenv from 'dotenv';
+import { App } from '@slack/bolt';
+import { initGather } from './gather';
 
-const API_KEY = "MQiaWBC2CYrRmUXW";
-const SPACE_ID = "c0ajLLNc3ph92hRG/Assembless";
-const MAP_ID = "rw-6"
+dotenv.config()
 
-const MIKE_PLAYER_ID = "1kZCHNAWcsZXqG1pZ9OzgFwX7jz2";
+console.log(process.env.SLACK_BOT_TOKEN);
 
-const main = async () => {
-    // create the game object, giving it your API key in this weird way
-    const game = new Game(SPACE_ID, () => Promise.resolve({ apiKey: API_KEY }), console.log);
-    // this is the line that actually connects to the server and starts initializing stuff
-    await game.connect();
-    // optional but helpful callback to track when the connection status changes
-    // game.subscribeToConnection((connected) => console.log("connected?", connected));
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
 
-    // game.subscribeToEvent('playerMoves', (v, x) => console.log(JSON.stringify(v, null, 2), JSON.stringify(x, null, 2)));
-    // game.teleport(MAP_ID, 24, 14, "1kZCHNAWcsZXqG1pZ9OzgFwX7jz2")
-    // game.fxShakeCamera(MAP_ID, MIKE_PLAYER_ID, 10, 2000)
-}
+(async () => {
+    const port = 3000
+    // Start your app
+    await app.start(process.env.PORT || port);
+    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 
-main();
+    const game = await initGather();
+
+    game.subscribeToEvent('playerChats', console.log)
+
+    app.client.chat.postMessage({ channel: 'office', text: 'Hello world!' });
+  })();
